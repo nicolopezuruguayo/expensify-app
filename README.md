f3be844dd822b9a11fd80056659f9a9e80cc4bba
Nico Lopez <nicolopezuruguayo@gmail.com>
2019-10-14 08:41:33 -0300

    commit/push with credentials

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Expensify-app
 
 The Complete React Developer Course (w/ Hooks and Redux)
 by Andrew Mead - excelent instructor by the way.
+
+git push -u origin master
--- src/app.js
+++ src/app.js
@@ -12,22 +12,13 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt:500}));
-store.dispatch(addExpense({description: 'Gas bill', amount: 3000, createdAt:1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 109500, createdAt:2000}));
-// store.dispatch(setTextFilter('Water'));
+// store.dispatch(addExpense({description: 'Water bill', amount: 4500, createdAt:500}));
+// store.dispatch(addExpense({description: 'Gas bill', amount: 3000, createdAt:1000}));
+// store.dispatch(addExpense({description: 'Rent', amount: 109500, createdAt:2000}));
 
-// setTimeout(() =>{
-//     store.dispatch(setTextFilter('Gas'));
-// }, 2000);
-
-// setTimeout(() =>{
-//     store.dispatch(setTextFilter('Cable TV'));
-// }, 4000);
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+// console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
