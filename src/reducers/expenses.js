614179edc1faef546398c53e5b349ab515df8f5c
Nico Lopez <nicolopezuruguayo@gmail.com>
2019-10-24 08:50:27 -0300

    Start Set Expenses

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -3,22 +3,24 @@
 const expensesReducerDefaultState = [];
 
 export default (state = expensesReducerDefaultState, action) => {
-    switch(action.type){
+    switch (action.type) {
         case 'ADD_EXPENSE':
             return [...state, action.expense];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
         case 'EDIT_EXPENSE':
             return state.map((expense) => {
-                if(expense.id === action.id){
+                if (expense.id === action.id) {
                     return {
                         ...expense,
                         ...action.updates
                     }
-                }else{
+                } else {
                     return expense;
                 }
             });
+        case 'SET_EXPENSES':
+            return action.expenses;
         default:
             return state;
     }
--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -2,7 +2,7 @@ import expensesReducer from '../../reducers/expenses';
 import expenses from '../fixtures/expenses';
 
 test('should set default state', () => {
-    const state = expensesReducer(undefined, { type: '@@INIT'});
+    const state = expensesReducer(undefined, { type: '@@INIT' });
     expect(state).toEqual([]);
 });
 
@@ -73,4 +73,13 @@ test('should not edit expense if id not found', () => {
     };
     const state = expensesReducer(expenses, action);
     expect(state).toEqual(expenses);
+});
+
+test('should set expenses', () => {
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses, action);
+    expect(state).toEqual([expenses[1]]);
 });
